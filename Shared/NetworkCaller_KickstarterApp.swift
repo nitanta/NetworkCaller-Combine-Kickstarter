28530a466752d4401cfa2a0052bbcc831c53b4d1
nitanta.adhikari <nitanta.adhikari@ebpearsl.com>
2021-04-21 16:39:48 +0545

    Initial Commit

---
+++ Shared/NetworkCaller_KickstarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCaller_KickstarterApp.swift
+//  Shared
+//
+//  Created by ebpearls on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCaller_KickstarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
